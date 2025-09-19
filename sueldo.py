143a9f4f2a2e6167fc324d6c945d77bb832f075b
Baryhdz <bruce.hernandez@iteso.mx>
2025-09-19 00:40:54 -0600

    prueba_sueldo

--- sueldo.py
+++ sueldo.py
@@ -4,13 +4,26 @@ Inserta el encabezado aquí y escribe tu código abajo
 
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+horas = int(input("Ingrese las horas trabajadas: "))
+tarifa = float(input("Ingrese la tarifa por hora: "))
 
 # Proceso
-
+if horas <= 48:
+    sueldo = horas * tarifa
+    horas_extras = 0
+    importe = 0
+else:
+    horas_extras = horas - 48
+    importe = horas_extras * tarifa * 2
+    sueldo = (48 * tarifa) + importe
 
 # Salidas
-print(salida)
+print(f"Horas trabajadas: {horas}")
+print(f"Tarifa por hora:{tarifa}" ) 
+
+print(f"Horas extras: {horas_extras}")
+print(f"Sueldo por horas extras: {importe}")
+print(f"Sueldo total: {sueldo}")
